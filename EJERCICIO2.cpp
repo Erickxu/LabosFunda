47434abb3fe5a3cf7cd837c415acce0f9cc349c4
Erickxu <angry.manz41@gmail.com>
2020-05-06 17:13:08 -0600

    Laboratorio 3

--- EJERCICIO2.cpp
+++ EJERCICIO2.cpp
@@ -8,7 +8,7 @@ using namespace std;
 int main()
 {
     float r = 0, a = 0, p = 0;
-    cout << "\n\tPor favor escriba el radio de un circulo para poder encontrar su area y su perimetro:" << endl;
+    cout << "\n\tPor favor escriba el radio de un circulo para poder encontrar su area y su perimetro:";
     cout << "\n\t"; cin >> r;
     a = M_PI * (pow(r, 2));
     p = 2 * M_PI * r;
--- EJERCICIO3.cpp
+++ EJERCICIO3.cpp
@@ -4,17 +4,17 @@ using namespace std;
 int main()
 {
     float a = 0, b = 0, c = 0, raiz = 0, x1 = 0, x2 = 0;
-    cout << "\tPor favor introduzca los 3 numeros de la ecuacion cuadratica para poder resolverlo mediante su formula: " << endl;
-    cout << "\tA: "; cin >> a;
+    cout << "\n\tPor favor introduzca los 3 numeros de la ecuacion cuadratica para poder resolverlo mediante su formula: " << endl;
+    cout << "\n\tA: "; cin >> a;
     cout << "\tB: "; cin >> b;
     cout << "\tC: "; cin >> c;
     raiz = pow(b, 2) - 4*a*c;
     if(raiz < 0)
-        cout << "\tEl problema no tiene solucion dentro de los reales." << endl;
+        cout << "\n\tEl problema no tiene solucion dentro de los reales." << endl;
     else{
         x1 = (-b + sqrt(raiz))/2*a;
         x2 = (-b - sqrt(raiz))/2*a;
-        cout << "\tLa respuesta es: " << endl;
+        cout << "\n\tLa respuesta es: " << endl;
         cout << "\n\tX1 = " << x1 << ", " << "X2 = " << x2 << endl;
     }
     return 0;
